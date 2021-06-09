b4e63d96811d7eda69b89e4c1209e8631041f4e1
foxelas <32893423+foxelas@users.noreply.github.com>
2021-06-10 01:49:14 +0900

    update

---
+++ src/matlab/scripts/plotCommonPlots.m
@@ -0,0 +1,22 @@
+close all;
+figure(1);
+hold on
+plot(lambdaIn, xyz(:,1), 'DisplayName','x');
+plot(lambdaIn, xyz(:,2), 'DisplayName','y');
+plot(lambdaIn, xyz(:,3), 'DisplayName','z');
+hold off 
+legend();
+SetSetting('plotName', fullfile(GetSetting('savedir'), '1_Common', 'interpColorMatchingFunctions'));
+title('Interpolated Color Matching Functions');
+xlabel('Wavelength (nm)');
+ylabel('Weight (a.u.)');
+savePlot(1);
+
+figure(2);
+plot(lambdaIn, illumination, 'DisplayName', 'Solax-iO');
+SetSetting('plotName', fullfile(GetSetting('savedir'), '1_Common', 'illumination'));
+title('Illumination');
+xlabel('Wavelength (nm)');
+ylabel('Radiant Intensity (a.u.)');
+legend();
+savePlot(2);
